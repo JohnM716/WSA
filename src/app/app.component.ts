78ca55e5cab64e5fbce881b20d4122dc0be31f74
John M Castañeda <jhoncastaneda128@gmail.com>
2026-09-04 13:52:43 -0500

    WSA - V01
    04/09/2026

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,8 +4,8 @@ import { RouterOutlet } from '@angular/router';
 @Component({
   imports: [RouterOutlet],
   selector: 'app-root',
-  styleUrl: './app.css',
-  templateUrl: './app.html',
+  styleUrl: './app.component.css',
+  templateUrl: './app.component.html',
 })
 export class App {
   protected readonly title = signal('WSA');
