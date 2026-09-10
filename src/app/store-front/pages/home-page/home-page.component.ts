47e8e53f7d6b15f9ffcbe2a268600c4810506204
John M Castañeda <jhoncastaneda128@gmail.com>
2026-09-10 18:47:03 -0500

    WSA - V01 - Creacion contacto
    10-09-2026

--- src/app/store-front/pages/home-page/home-page.component.ts
+++ src/app/store-front/pages/home-page/home-page.component.ts
@@ -8,5 +8,24 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 })
 export class HomePageComponent {
 
+  async enviarFormulario(event: Event) {
+    event.preventDefault();
+    const form = event.target as HTMLFormElement;
+    const data = new FormData(form);
+
+    const response = await fetch('https://formspree.io/f/xvkolqwk', {
+      method: 'POST',
+      body: data,
+      headers: {
+        'Accept': 'application/json'
+      }
+    });
+
+    if (response.ok) {
+      form.reset();
+      const modal = document.getElementById('modal_exito') as HTMLDialogElement;
+      modal?.showModal();
+    }
+  }
 
 }
